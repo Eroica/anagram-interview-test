347ccb599aae66295f51f13e26d3ecbb6128dfed
Sebastian <sebastians@mailbox.org>
2023-04-23 17:38:24 +0200

    Provide default values outside of component

--- src/main.js
+++ src/main.js
@@ -2,4 +2,8 @@ import * as riot from "riot";
 import App from "./App.riot";
 
 const mountApp = riot.component(App);
-const app = mountApp(document.getElementById("App"));
+const app = mountApp(document.getElementById("App"), {
+	word1: "Word 1",
+	word2: "Word 2",
+	isCaseInsensitive: true
+});
