fb7faf6610defed4c59d1dbecbc53b86103ff43e
Sebastian <sebastians@mailbox.org>
2023-04-24 15:38:38 +0200

    Better explain Collator usage with (sample) German locale

--- src/anagram.js
+++ src/anagram.js
@@ -15,16 +15,20 @@ export default function checkIsAnagram (word1, word2, isCaseInsensitive) {
 		return false;
 	}
 
-	/* Using locale-aware sorting */
-	const collator = new Intl.Collator();
+	/* Using a Collator here allows us to be more lenient when comparing
+	 * letters. For example, (German) users might enter "Lager" and "Regal",
+	 * and in everyday speech these are considered anagrams even though the
+	 * case of r/R and l/L doesn't match.
+	 * In a more strict version, the case of a letter could be taken into
+	 * account; this might be necessary for languages such as Turkish where
+	 * lower-casing `I` has locale-dependent results. */
+	const collator = new Intl.Collator(
+		"de",
+		{ sensitivity: isCaseInsensitive ? "base" : "variant" }
+	);
 
-	return word1.split("").sort(collator.compare).join("").localeCompare(
-		word2.split("").sort(collator.compare).join(""),
-		{},
-		/* To allow comparisons like "Lager" and "Regal", act as if all letters were
-		 * entered in lower case. In a more strict version, the case of a letter could be
-		 * taken into account; this might be necessary for languages such as Turkish where
-		 * lower-casing `I` has locale-dependent results. */
-		{ sensitivity: isCaseInsensitive ? "base" : "case" }
+	return collator.compare(
+		word1.split("").sort(collator.compare).join(""),
+		word2.split("").sort(collator.compare).join("")
 	) === 0;
 }
